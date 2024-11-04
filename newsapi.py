999d7839da961600a954d5e882a8d816b07f6cba
HariHaran Janardhanan <hariharanjanardhanan@10-21-202-180.dynapool.wireless.nyu.edu>
2024-11-04 03:55:48 -0500

    newsapi-location city two methods scope and relevence

--- newsapi.py
+++ newsapi.py
@@ -1,119 +1,112 @@
-import requests
+# 2
+# _
+# _
+# .
 
+
+import requests
+import time
+import requests
+from pprint import pprint
 username = "hj2342@nyu.edu"
 password = "nirESV96Te!$6Ar"
 AppID = "340a6381"
 
-token = requests.post("https://api.aylien.com/v1/oauth/token", 
-                     auth=(username, password), 
-                     data={"grant_type": "password"}).json()["access_token"]
 
-headers = {"Authorization": "Bearer {}".format(token), "AppId": AppID}
 
-url = 'https://api.aylien.com/v6/news/stories?aql=entities:({{surface_forms.text:"Chennai" AND overall_prominence:>=0.65}}) AND sentiment.title.polarity:(negative neutral positive)&cursor=*&published_at.end=NOW&published_at.start=NOW-7DAYS/DAY'
+def get_auth_header(username, password, appid):
+    # Generate the authorization header for making requests to the Aylien API.
 
-response = requests.get(url, headers=headers)
-responses = response.json()
+    token = requests.post('https://api.aylien.com/v1/oauth/token', auth=(username, password), data={'grant_type': 'password'})
 
-def similar_titles(title1, title2, threshold=0.5):
-    # Convert titles to sets of words
-    words1 = set(title1.lower().split())
-    words2 = set(title2.lower().split())
-    
-    # Calculate overlap
-    common_words = words1.intersection(words2)
-    total_words = max(len(words1), len(words2))
-    
-    # Calculate similarity ratio
-    similarity = len(common_words) / total_words
-    return similarity > threshold
+    token = token.json()['access_token']
 
-# Use a list to maintain order and check for duplicates
-unique_titles = []
-count = 1
+    headers = {f'Authorization': 'Bearer {}'.format(token), 'AppId': appid}
 
-for story in responses['stories']:
-    title = story['title'].strip()
-    is_duplicate = False
-    
-    # Check against existing titles
-    for existing_title in unique_titles:
-        if similar_titles(title, existing_title):
-            is_duplicate = True
+    return headers
+
+
+def get_top_stories(params, headers, n_top_stories=False):
+    fetched_stories = []
+    stories = None
+
+    if 'per_page' in params.keys():
+        if params['per_page'] > n_top_stories and not n_top_stories == False:
+            params['per_page'] = n_top_stories
+
+    while (
+        stories is None
+        or len(stories) > 0
+        and (len(fetched_stories) < n_top_stories or n_top_stories == False)
+    ):
+
+        try:
+            response = requests.get('https://api.aylien.com/v6/news/stories', params=params, headers=headers)
+
+            # If the call is successfull it will append it
+            if response.status_code == 200:
+                response_json = response.json()
+                stories = response_json['stories']
+
+                if 'next_page_cursor' in response_json.keys():
+                    params['cursor'] = response_json['next_page_cursor']
+                else:
+                    pprint('No next_page_cursor')
+
+                fetched_stories += stories
+
+                if len(stories) > 0 and not stories == None:
+                    print(
+                        'Fetched %d stories. Total story count so far: %d'
+                        % (len(stories), len(fetched_stories))
+                    )
+
+            # If the application reached the limit per minute it will sleep and retry until the limit is reset
+            elif response.status_code == 429:
+                time.sleep(10)
+                continue
+
+            # If the API call face network or server errors it sleep for few minutes and try again a few times until completely stop the script.
+            elif 500 <= response.status_code <= 599:
+                time.sleep(260)
+                continue
+
+            # If the API call return any other status code it return the error for futher investigation and stop the script.
+            else:
+                pprint(response.text)
+                break
+
+        except Exception as e:
+            # In case the code fall in any exception error.
+            pprint(e)
             break
-    
-    if not is_duplicate:
-        print(f"{count}. {title}")
-        unique_titles.append(title)
-        count += 1
-# import requests
-# import time
-# import requests
-# from pprint import pprint
 
-# username = "hj2342@nyu.edu"
-# password = "nirESV96Te!$6Ar"
-# AppID = "340a6381"
+    return fetched_stories
+headers = get_auth_header(username, password, AppID)
+city = "London"  # You can change this to any city you want
 
+params = {
+    "published_at": "[NOW-14DAYS/HOUR TO NOW/HOUR]",
+    "language": "(en)",
+    "entities": '{{element:title AND surface_forms:"' + city + '" AND type:("Location", "City")}}',
+    "sort_by": "published_at",
+    "per_page": 100,
+}
 
 
-# def get_auth_header(username, password, appid):
-#     # Generate the authorization header for making requests to the Aylien API.
+# # Define lists of countries and cities
+# country = "GB"
 
-#     token = requests.post('https://api.aylien.com/v1/oauth/token', auth=(username, password), data={'grant_type': 'password'})
-
-#     token = token.json()['access_token']
-
-#     headers = {f'Authorization': 'Bearer {}'.format(token), 'AppId': appid}
-
-#     return headers
-
-
-# def get_clusters(params, headers):
-#     #  Make a GET request to the Aylien News API to retrieve news clusters based on the provided parameters and headers.
-
-#     while True:
-#         try:
-#             response = requests.get('https://api.aylien.com/v6/news/clusters', params=params, headers=headers)
-
-#             # If the call is successfull it will append it
-#             if response.status_code == 200:
-#                 fetched_clusters = response.json()
-#                 return fetched_clusters['clusters']
-
-#             # If the application reached the limit per minute it will sleep and retry until the limit is reset
-#             elif response.status_code == 429:
-#                 time.sleep(10)
-#                 continue
-
-#             # If the API call face network or server errors it sleep for few minutes and try again a few times until completely stop the script.
-#             elif 500 <= response.status_code <= 599:
-#                 time.sleep(260)
-#                 continue
-
-#             # If the API call return any other status code it return the error for futher investigation and stop the script.
-#             else:
-#                 pprint(response.text)
-#                 break
-
-#         except Exception as e:
-#             # In case the code fall in any exception error.
-#             print(e)
-#             break
-# headers = get_auth_header(username, password, AppID)
 
 # params = {
-#     'story_count': '[20 TO 100]',
-#     'earliest_story.start': '2023-09-01T00:00:00Z',
-#     'earliest_story.end': '2023-09-01T23:59:59Z',
-#     'latest_story.start': '2023-09-14T00:00:00Z',
-#     'latest_story.end': '2023-09-14T23:59:59Z',
-#     'location.country': 'US',
-
+#     "published_at": "[NOW-14DAYS/HOUR TO NOW/HOUR]",
+#     "language": "(en)",
+#     "source.scopes.country": '(' + country + ')',
+#     "source.scopes.city": '("' + city + '")',
+#     "sort_by": "published_at",
+#     "per_page": 100,
 # }
 
-# clusters = get_clusters(params, headers)
-# for i in clusters:
-#     print(i)
-    
-    
+stories = get_top_stories(params, headers, 100)
+for i in range(5):
+    print(i, stories[i]['title'], end='\n')
