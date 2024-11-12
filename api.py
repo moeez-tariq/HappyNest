e74d4111013b02166fcf9c973faa168cf00d2dd6
HariHaran Janardhanan <hariharanjanardhanan@10-21-210-41.dynapool.wireless.nyu.edu>
2024-11-12 02:45:23 -0500

    news by city

--- api.py
+++ api.py
@@ -7,19 +7,10 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime
 from typing import Optional, Dict, List
-import time
-import requests
-from pprint import pprint
-from difflib import SequenceMatcher
 
 
 # Load environment variables
 load_dotenv()
-
-# Access environment variables
-USERNAME = os.getenv("AYLIEN_USERNAME")
-PASSWORD = os.getenv("AYLIEN_PASSWORD")
-APP_ID = os.getenv("AYLIEN_APP_ID")
 
 app = FastAPI()
 origins = [
@@ -108,91 +99,6 @@ def str_to_objectid(id_str: str) -> ObjectId:
         return ObjectId(id_str)
     except Exception:
         raise HTTPException(status_code=400, detail="Invalid ObjectId format")
-    
-def get_auth_header(username, password, appid):
-    # Generate the authorization header for making requests to the Aylien API.
-
-    token = requests.post('https://api.aylien.com/v1/oauth/token', auth=(username, password), data={'grant_type': 'password'})
-
-    token = token.json()['access_token']
-
-    headers = {f'Authorization': 'Bearer {}'.format(token), 'AppId': appid}
-
-    return headers
-
-# Function to calculate similarity
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-
-# Function to remove duplicates based on a given threshold
-def remove_duplicates(stories, threshold=0.5):
-    unique_stories = []
-    seen_titles = []
-
-    for story in stories:
-        title = story['title']
-        if not any(similar(title, seen_title) > threshold for seen_title in seen_titles):
-            unique_stories.append(story)
-            seen_titles.append(title)
-    return unique_stories
-
-def get_top_stories(params, headers, n_top_stories=False):
-    fetched_stories = []
-    stories = None
-
-    if 'per_page' in params.keys():
-        if params['per_page'] > n_top_stories and not n_top_stories == False:
-            params['per_page'] = n_top_stories
-
-    while (
-        stories is None
-        or len(stories) > 0
-        and (len(fetched_stories) < n_top_stories or n_top_stories == False)
-    ):
-
-        try:
-            response = requests.get('https://api.aylien.com/v6/news/stories', params=params, headers=headers)
-
-            # If the call is successfull it will append it
-            if response.status_code == 200:
-                response_json = response.json()
-                stories = response_json['stories']
-
-                if 'next_page_cursor' in response_json.keys():
-                    params['cursor'] = response_json['next_page_cursor']
-                else:
-                    pprint('No next_page_cursor')
-
-                fetched_stories += stories
-
-                if len(stories) > 0 and not stories == None:
-                    print(
-                        'Fetched %d stories. Total story count so far: %d'
-                        % (len(stories), len(fetched_stories))
-                    )
-
-            # If the application reached the limit per minute it will sleep and retry until the limit is reset
-            elif response.status_code == 429:
-                time.sleep(10)
-                continue
-
-            # If the API call face network or server errors it sleep for few minutes and try again a few times until completely stop the script.
-            elif 500 <= response.status_code <= 599:
-                time.sleep(260)
-                continue
-
-            # If the API call return any other status code it return the error for futher investigation and stop the script.
-            else:
-                pprint(response.text)
-                break
-
-        except Exception as e:
-            # In case the code fall in any exception error.
-            pprint(e)
-            break
-
-    return fetched_stories
-
 
 # User Endpoints
 @app.post("/api/users/")
@@ -326,44 +232,6 @@ async def home(name:str):
             raise HTTPException(status_code=404, detail="News article not found")
     return {"data":news_articles}
     
-
-
-@app.get("/api/news/fetch", response_model=List[NewsArticle])
-async def fetch_news():
-    headers = get_auth_header(USERNAME, PASSWORD, APP_ID)
-    city = "New York"
-
-    params = {
-        "published_at": "[NOW-14DAYS/HOUR TO NOW/HOUR]",
-        "language": "(en)",
-        "entities": '{{element:title AND surface_forms:"' + city + '" AND type:("Location", "City")}}',
-        "sort_by": "published_at",
-        "per_page": 100,
-    }
-
-    stories = get_top_stories(params, headers, 100)
-    # Remove duplicates with the threshold of 50%
-    deduplicated_stories = remove_duplicates(stories, threshold=0.5)
-
-    for story in deduplicated_stories:
-        news_data = {
-            "title": story["title"],
-            "content": story["body"],
-            "location": {
-                "city": city,
-                "state": "New York",
-                "country": "United States",
-                "coordinates": {
-                    "lat": 40.7128,
-                    "lon": -74.0060
-                }
-            },
-            "sentiment": story["sentiment"]["body"]["polarity"],
-            "published_at": story["published_at"],
-            "source": story["source"]["name"]
-        }
-        if news_data["sentiment"] == "positive":
-            news_collection.insert_one(news_data)
 
 
 @app.get("/api/news/{article_id}", response_model=NewsArticle)
